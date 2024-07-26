aa20c7a44d18a11733ea7312dabd4fec4b2ecee9
ezpnix <139456596+ezpnix@users.noreply.github.com>
2024-07-26 12:55:53 +0800

    Add files via upload

---
+++ Swiper.js
@@ -0,0 +1,10 @@
+   <script>
+   var swiper = new Swiper('.swiper-container', {
+       slidesPerView: 3, // Show 3 articles per slide
+       spaceBetween: 30, // Add spacing between articles
+       pagination: {
+           el: '.swiper-pagination',
+           clickable: true,
+       },
+   });
+   </script>
