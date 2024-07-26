9785c3e6632d74e34b79479a10f7fcd86dd980a9
ezpnix <139456596+ezpnix@users.noreply.github.com>
2024-07-26 13:22:56 +0800

    Update Swiper.js

--- Swiper.js
+++ Swiper.js
@@ -1,7 +1,6 @@
 <script>
 var swiper = new Swiper('.swiper-container', {
-    direction: 'vertical', // Set swiper direction to vertical
-    slidesPerView: 'auto', // Automatically adjust the number of articles per slide
+    slidesPerView: 3, // Show 3 articles per slide
     spaceBetween: 30, // Add spacing between articles
     pagination: {
         el: '.swiper-pagination',
